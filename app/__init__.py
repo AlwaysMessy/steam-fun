e9d4796738ad6df9534d24ce787acc8f45206a31
凌晓斌 <18819427451@163.com>
2020-01-18 11:00:01 +0800

    'change'

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ def create_app(config_name=None):
         #如果没有传入配置名，就从FLASK_CONFIG环境变量获取，如果没有获取到则使用默认值development
         config_name = os.getenv('FLASK_CONFIG', 'development')
 
-        app = Flask(__name__)
+        app = Flask('wsgi')
         app.config.from_object(config[config_name])
 
         db.init_app(app)
